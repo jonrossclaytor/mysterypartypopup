33e32d4d061678577ee9b076bafbed28eb1c2351
Ross Claytor <johnrossclaytor@gmail.com>
2017-12-11 15:54:44 -0600

    2nd

--- mysterypartypopup.py
+++ mysterypartypopup.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'no news is good news'
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
